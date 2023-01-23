47436133e1492f24eab3e47ad5f3238e13a676b2
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-22 21:30:29 -0800

    add scrapper for leaderboard

---
+++ scraping/index.js
@@ -0,0 +1,40 @@
+import * as cheerio from 'cheerio'
+
+const urls = {
+  leaderBoard: 'https://kingsleague.pro/estadisticas/clasificacion/'
+}
+
+async function scrape (url) {
+  const res = await fetch(url)
+  const html = await res.text()
+  return cheerio.load(html)
+}
+
+async function getLeaderBoard () {
+  const $ = await scrape(urls.leaderBoard)
+  const $rows = $('table tbody tr')
+  const leaderBoard = []
+  const leaderBoardSelectors = {
+    team: { selector: '.fs-table-text_3', typeOf: 'string' },
+    wins: { selector: '.fs-table-text_4', typeOf: 'number' },
+    loses: { selector: '.fs-table-text_5', typeOf: 'number' },
+    goalsScored: { selector: '.fs-table-text_6', typeOf: 'number' },
+    goalsConceded: { selector: '.fs-table-text_7', typeOf: 'number' },
+    yellowCards: { selector: '.fs-table-text_8', typeOf: 'number' },
+    redCards: { selector: '.fs-table-text_9', typeOf: 'number' }
+  }
+
+  $rows.each((i, el) => {
+    const leaderBoardEntries = Object.entries(leaderBoardSelectors).map(([key, { selector, typeOf }]) => {
+      const rowValue = $(el).find(selector).text().trim()
+      const value = typeOf === 'number' ? Number(rowValue) : rowValue
+      return [key, value]
+    })
+    leaderBoard.push(Object.fromEntries(leaderBoardEntries))
+  })
+
+  return leaderBoard
+}
+
+const leaderBoard = await getLeaderBoard()
+console.log(leaderBoard)
