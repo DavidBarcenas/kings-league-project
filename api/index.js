af019228b1bff0a8d49200f9bb104a598e85cf79
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-24 21:30:55 -0800

    update image path and add endpoint for statics

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.json([
-    {
-      endpoint: '/leaderboard',
-      description: 'Returns the leaderboard'
-    }
-  ])
-})
+app.get('/', (c) => c.json([
+  {
+    endpoint: '/leaderboard',
+    description: 'Returns the leaderboard'
+  }
+]))
 
 app.get('/leaderboard', (c) => {
   return c.json(leaderboard)
 })
+
+app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
