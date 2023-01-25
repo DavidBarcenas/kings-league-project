8e76beca8164388ce67feb80a91028edbbcf64be
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-24 23:10:58 -0800

    add new endpoints

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
+import teams from '../db/teams.json'
+import presidents from '../db/presidents.json'
 
 const app = new Hono()
 
@@ -8,13 +10,26 @@ app.get('/', (c) => c.json([
   {
     endpoint: '/leaderboard',
     description: 'Returns the leaderboard'
+  },
+  {
+    endpoint: '/teams',
+    description: 'Returns the teams'
+  },
+  {
+    endpoint: '/presidents',
+    description: 'Returns the presidents'
   }
 ]))
 
-app.get('/leaderboard', (c) => {
-  return c.json(leaderboard)
-})
+app.get('/leaderboard', (c) => c.json(leaderboard))
+app.get('/teams', c => c.json(teams))
+app.get('/presidents', c => c.json(presidents))
+app.get('/presidents/:id', c => {
+  const id = c.req.param('id')
+  const president = presidents.find(president => president.id === id)
 
+  return president ? c.json(president) : c.json({ message: 'President not found' }, 404)
+})
 app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
