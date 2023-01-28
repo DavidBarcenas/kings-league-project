d29a3cf053cf834e0f83968d8266e68109bf9d86
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-28 15:46:56 -0800

    update prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: ['*.json', '*.md', '*.toml', '*.yml'],
+      options: {
+        useTabs: false
+      }
+    }
+  ],
+  endOfLine: 'lf'
+}
