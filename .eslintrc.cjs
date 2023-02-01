8104b562968317a72c1e96199e730bfc775c92f2
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-31 22:12:38 -0600

    remove eslint from package.json

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['standard', 'plugin:astro/recommended'],
+  rules: {
+    'no-tabs': 'off',
+    indent: 'off',
+    'space-before-function-paren': 'off'
+  },
+  // ...
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {}
+    }
+  ]
+}
