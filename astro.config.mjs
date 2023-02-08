a5f27f0abd7c5fcf097ca0d9400d38d4912bfe0d
David Barcenas <davidbarcenasmx@gmail.com>
2023-02-08 15:10:44 -0600

    add meta tags in layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'website-url',
+  // site: 'website-url',
   server: {
     host: true
   },
