051f3c48d467e5332bf228c1c75ca7f80191e6f0
David Barcenas <davidbarcenasmx@gmail.com>
2023-02-07 20:56:50 -0600

    setup astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,22 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
+import critters from 'astro-critters'
+import prefetch from '@astrojs/prefetch'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'website-url',
+  server: {
+    host: true
+  },
+  integrations: [
+    tailwind(),
+    critters(),
+    prefetch(),
+    sitemap({
+      lastmod: new Date()
+    })
+  ]
 })
