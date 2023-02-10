8d4764ad8a2e5695904ed49bd2a017f5dd27a4cb
David Barcenas <davidbarcenasmx@gmail.com>
2023-02-09 21:50:12 -0600

    add leaderboard component

--- src/services/leaderboard.js
+++ src/services/leaderboard.js
@@ -1,10 +1,20 @@
+import { apiURL } from './config.js'
+
+export const getLeaderboard = async () => {
+  try {
+    const response = await fetch(`${apiURL}/leaderboard`)
+    const leaderboard = await response.json()
+    return leaderboard
+  } catch (e) {
+    // enviar el error a un servicio de reporte de errores
+    return null
+  }
+}
+
 export const findLeaderboardBy = async ({ teamId }) => {
   try {
-    const response = await fetch(
-      `https://kings-league-api.daveeb.workers.dev/leaderboard/${teamId}`
-    )
+    const response = await fetch(`${apiURL}/leaderboard/${teamId}`)
     const teamStats = await response.json()
-
     return teamStats
   } catch (e) {
     // enviar el error a un servicio de reporte de errores
